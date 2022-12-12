e66feb01c6aa1c0eb3d37ed26c92a771e70853fc
ezequielsousa <ezequiel.sousa@yahoo.com>
2022-12-12 15:30:34 -0300

    Modificando pesquisa
    Alteração de pesquisa

--- search.sh
+++ search.sh
@@ -2,10 +2,11 @@
 
 function search_find(){
     echo "Inicializando a pesquisa para usuário: "$(whoami)
+    echo "Local de pesquisa atual: "$PWD
     
-    _search=$(find . -type d,f -name *.iso -mtime 30 -user $(whoami) -size 4G)
+    _search=$(find . -type d,f -perm /u+rw,g+rw,o+x -mtime +30 -user $(whoami) -size 4G)
     
-    if [ _search -eq 1 ]; then
+    if [ _search = 0 ]; then
         echo "Encontramos arquivos com mais de 4 gigabytes em seu pc."
     else
         echo "Não conseguimos encontrar nada com mais de 30 dias no seu pc ocupando espaço."

8d6564547b5b59ccc8a6255aa0b27f8dddf2327c
ezequielsousa <ezequiel.sousa@yahoo.com>
2022-12-14 00:42:51 -0300

    Adicionando Informações Uteis
    Correções e adicionado uma variável que informa a data de pesquisa

--- search.sh
+++ search.sh
@@ -3,13 +3,17 @@
 function search_find(){
     echo "Inicializando a pesquisa para usuário: "$(whoami)
     echo "Local de pesquisa atual: "$PWD
+
+    date_format=$(date '+%d/%m/%Y')
+
+    printf "data de hoje é: "$date_format"\n"
     
-    _search=$(find . -type d,f -perm /u+rw,g+rw,o+x -mtime +30 -user $(whoami) -size 4G)
-    
-    if [ _search = 0 ]; then
-        echo "Encontramos arquivos com mais de 4 gigabytes em seu pc."
+    _searchp=$(find . -type d,f -perm /u+rw,g+rw,o+x -mtime +30 -user $(whoami) -size 4G)
+
+    if [ $_searchp ]; then
+        printf "Encontramos arquivos com mais de 4 gigabytes em seu pc a mais de 30 dias ocupando espaço em disco."
     else
-        echo "Não conseguimos encontrar nada com mais de 30 dias no seu pc ocupando espaço."
+        printf "Não conseguimos encontrar nada com mais de 30 dias no seu pc ocupando espaço."
     fi
 }
 search_find

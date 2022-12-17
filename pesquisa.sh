148c00a6e5e1d56096536b921210e2ccac31c920
Ezequiel Sousa <ezequiel.sousa@yahoo.com>
2022-12-17 12:43:36 -0300

    alterações para pesquisa

---
+++ pesquisa.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+# --------------------------------------------------------------------
+# Autor: Ezequiel Sousa
+# Profissão: Operador de Computador
+# ./pesquisa.sh
+# Desde: sex dez 16 13:13:00 BRT 2022
+# Versão: 1.0
+# Licença: MIT
+# Script usando comando find
+# Verifica o diretório do usuário buscando por arquivos e dirétórios
+# Com + de 30 dias que possuem tamanho de 4096 Megabytes.
+# --------------------------------------------------------------------
+LC_TIME=pt_BR.UTF-8
+
+pesquisa(){
+    # Informações de $USER e $PATH
+    echo "Inicializando a pesquisa para usuário: "$(whoami)
+    echo "Local de pesquisa atual: "$PWD
+
+    # Formata a data atual para pt-br
+    date_format=$(date '+%d/%m/%Y')
+
+    printf "data de hoje é: "$date_format"\n"
+    
+    checar=$(find ~/ \
+        -type d,f \
+        -mtime +30 \
+        -user $(whoami) \
+        -size +4096M \
+        -fprint /tmp/pesquisa.log)
+
+    listen_log=$(/usr/bin/cat /tmp/pesquisa.log)
+    verifica=$(du -m /tmp/pesquisa.log)
+
+    # Verifica o arquivo de log e imprime a saída se verdadeiro
+    # Caso contrario, deleta o arquivo com 0 bytes.
+    if [[ -d /tmp/pesquisa.log ]];
+    then
+        printf "Encontramos algo para voce! Listando.. Arquivo de log: /tmp/pesquisa.log\n $listen_log"
+    else
+        delete_log=$(/usr/bin/rm /tmp/pesquisa.log)
+        printf "Parece que está tudo certo!\n"
+        printf "Deletando arquivo de log vazio... /tmp/pesquisa.log\n"
+    fi
+}
+pesquisa
